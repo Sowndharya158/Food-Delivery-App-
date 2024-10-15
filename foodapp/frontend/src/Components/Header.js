6a843395dd13111dc11f66c1b772a717f72f8fde
Sowndharya158 <69009510+Sowndharya158@users.noreply.github.com>
2024-10-15 20:33:23 +0530

    Update Header.js
    updated heading from h1 to h2

--- foodapp/frontend/src/Components/Header.js
+++ foodapp/frontend/src/Components/Header.js
@@ -14,7 +14,7 @@ navigate("/cart")
     return(
         <header><div className="header">
             <span style={{display:"flex", gap:"1em"}}><img className="logo" src="/logo.jpg" alt="logo" />
-            <h1>{content}</h1></span>
+            <h2>{content}</h2></span>
              <><div style={{display:"flex",gap:"1rem"}}>
              <h2 onClick={handleHome} style={{cursor:"pointer",color:"#A06C23",fontSize:"1.5em",marginRight:"1rem",marginTop:"1em"}}>Home <i onClick={handleHome} className="fa-solid fa-house" style={{color:"#A06C23",cursor:"pointer"}}/></h2>
                 <h2 onClick={handleCart} style={{cursor:"pointer",color:"#A06C23",fontSize:"1.5em",marginRight:"1rem",marginTop:"1em"}}>Cart <i onClick={handleCart} className="fa fa-shopping-bag" style={{color:"#A06C23",cursor:"pointer"}}/></h2><h2 style={{color:"#A06C23",fontSize:"1.5em",marginRight:"1rem",marginTop:"1em",cursor:"pointer",}} onClick={handleOrderDetails}>Orders <i className="fa-solid fa-truck" style={{color:"#A06C23",cursor:"pointer"}} onClick={handleOrderDetails}/></h2></div></>
